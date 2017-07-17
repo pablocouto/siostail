d0cde002a7ff5fe9e107d5455915e5937486f538
Pablo Couto <pablo@uncharted.space>
2017-07-17 20:21:16 +0200

    Refactor

--- tests/dha.rs
+++ tests/dha.rs
@@ -18,61 +18,56 @@ extern crate siostail;
 
 use chrono::TimeZone;
 use chrono::offset::Local;
-use siostail::error::Error;
-use siostail::{Endpoint, Esios, Token, day_range_rfc3339};
+use siostail::day_range_rfc3339;
+use siostail::error::{Error, Result};
+use siostail::{Endpoint, Esios, Token};
 use std::error::Error as StdError;
 
 // TODO: Obtain from environment, to ease use with Travis.
 // NB: Generated in browser; subject to expiration.
 const TOKEN: &str = "8f38cf52b8b1e583a80f6af92ec58d07f343d32e51ab96f9ae9d674692c9e51d";
 
-struct Helper {}
+struct Helper;
 
 impl Helper {
-    fn endpoint() -> Endpoint {
+    fn endpoint() -> Result<Endpoint> {
         let token = Token(TOKEN.to_string());
         let timeout = 5;
-        Endpoint::new(token, timeout).unwrap()
+        Endpoint::new(token, timeout)
+    }
+
+    fn handle_err(error: Option<Error>) {
+        match error {
+            None => return (),
+            // Server timeouts are OK.
+            Some(Error::Timeout) => {
+                println!("{}", Error::Timeout.description());
+                return ();
+            }
+            Some(ref err) => {
+                panic!("{:?}", err);
+            }
+        }
     }
 }
 
 #[test]
 #[ignore] // Possibly too demanding on the server.
 fn indicators() {
-    let mut endpoint = Helper::endpoint();
-    let res = endpoint.indicators();
-    match res.err() {
-        None => return (),
-        // Server timeouts are OK:
-        Some(Error::Timeout) => {
-            println!("{}", Error::Timeout.description());
-            return ();
-        }
-        Some(ref err) => {
-            panic!("{:?}", err);
-        }
-    }
+    let mut esios = Helper::endpoint().unwrap();
+    let res = esios.indicators();
+    Helper::handle_err(res.err());
 }
 
 #[test]
 fn indicator() {
-    let mut endpoint = Helper::endpoint();
+    let mut esios = Helper::endpoint().unwrap();
     let (start_time, end_time) = day_range_rfc3339(Local.ymd(2014, 04, 01));
-    let res = endpoint.indicator(&start_time, &end_time);
+    let res = esios.indicator(&start_time, &end_time);
     let res = res.map(|data| {
         let Esios::Indicator { values, .. } = data;
         // Externally known value:
         assert_eq!(values[0].value, 40.55);
     });
-    match res.err() {
-        None => return (),
-        // Server timeouts are OK:
-        Some(Error::Timeout) => {
-            println!("{}", Error::Timeout.description());
-            return ();
-        }
-        Some(ref err) => {
-            panic!("{:?}", err);
-        }
-    }
+    Helper::handle_err(res.err());
 }
